3ab31900dbd5b95d57a83982274979289e84a595
camilevahviraki <camilevahviraki@gmail.com>
2022-04-28 22:49:58 +0200

    Add files in src folder

---
+++ src/modules/addAndRemove.js
@@ -0,0 +1,80 @@
+
+const checkItem = localStorage.getItem('TaskToday');
+let toDoList = [];
+
+  
+
+class UserTask {
+    constructor(title1, author1, id1) {
+      this.description = title1;
+      this.completed = author1;
+      this.index = id1;
+    }
+  
+    add() {
+      if (this.title === '') {
+        alert('Fill all inputs');
+      } else if (!checkItem) {
+        toDoList.push(this);
+        localStorage.setItem('TaskToday', JSON.stringify(toDoList));
+        console.log(toDoList);
+         location.reload();
+      } else {
+        const ArrayStored = localStorage.getItem('TaskToday');
+        const ArrayStoredParse = JSON.parse(ArrayStored);
+        ArrayStoredParse.push(this);
+        localStorage.setItem('TaskToday', JSON.stringify(ArrayStoredParse));
+        toDoList = JSON.parse(ArrayStored);
+         location.reload();
+        console.log(toDoList);
+      }
+    }
+
+    updateStore() {
+        const ArrayStored = localStorage.getItem('TaskToday');
+        const ArrayStoredParse = JSON.parse(ArrayStored);
+
+        const inputs = document.querySelectorAll('.inputTask');
+        inputs.forEach((element,index) => {
+            element.addEventListener('keypress', (event) => {
+                if (event.key === 'Enter') {
+                  event.preventDefault();
+    
+                  ArrayStoredParse.forEach((a, i) => {
+                    const listUpdate = document.getElementById(i);
+                    ArrayStoredParse[i].description = listUpdate.value;
+                    localStorage.setItem('TaskToday', JSON.stringify(ArrayStoredParse));
+                    location.reload();
+                  });
+                  
+                }
+            });
+            
+        });
+
+    }
+
+    updateId() {
+        const TasksR = JSON.parse(localStorage.getItem('TaskToday'));
+        TasksR.forEach((a,i) => {
+            a.index = i;
+            localStorage.setItem('TaskToday', JSON.stringify(TasksR));
+        })
+    }
+  
+    removeTask() {
+      const removeList = document.querySelectorAll('.imgRemove');
+      const BookStored = JSON.parse(localStorage.getItem('TaskToday'));
+      removeList.forEach((a, i) => {
+        document.getElementById('imtrash'+i).addEventListener('click', () => {
+          const BookFiltered = BookStored.filter((book, index) => book.index !== i);
+          localStorage.setItem('TaskToday', JSON.stringify(BookFiltered));
+          location.reload();
+          this.updateId();
+        });
+      });
+    }
+
+}
+
+export default UserTask;
---
+++ src/modules/wrapContainer.js
@@ -0,0 +1,13 @@
+import imgRecycle from '../images/recycle.png';
+
+const WrapContainer = () => {
+    document.body.innerHTML = `<div class="container">
+   <h3>Today's to do
+   <img src=${imgRecycle} id="imgRecycle" alt=""/></h3>
+   <input type="text" placeholder="add your list..." id="inputD" draggable="true"/>
+   <ul id="list"></ul>
+   <button class="button_clear" id="btnRemove">Clear all selected</button>
+</div>`;
+}
+
+export default WrapContainer;
