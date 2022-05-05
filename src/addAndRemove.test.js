a431cdd683e2f3b8674d7072c4db4421150d595a
camilevahviraki <camilevahviraki@gmail.com>
2022-05-05 20:12:41 +0200

    Add babelrc file and intall dependencie

--- src/addAndRemove.test.js
+++ src/addAndRemove.test.js
@@ -1,5 +1,5 @@
-const jsdom = require('jsdom');
-const UserTask = require('../__mocks__/addAndRemove.js');
+import jsdom from 'jsdom';
+import UserTask from '../__mocks__/addAndRemove.js';
 
 const { JSDOM } = jsdom;
 const dom = new JSDOM('<!DOCTYPE html>');
@@ -33,5 +33,59 @@ describe('test localStorage remove', () => {
   });
   test('remove when length = null', () => {
     expect(addAndRemove.removeTask(3)).toBe(0);
+  });
+});
+
+describe('Remove checked tasks', () => {
+  test('add elmnts check and remove checkeds elmnts', () => {
+    const addAndRemove1 = new UserTask('camilux', false, 0);
+    addAndRemove1.add();
+    const addAndRemove2 = new UserTask('camilux1', false, 1);
+    addAndRemove2.add();
+    const addAndRemove3 = new UserTask('camilux2', false, 2);
+    addAndRemove3.add();
+    const addAndRemove4 = new UserTask('camilux3', false, 3);
+    addAndRemove4.add();
+    const addAndRemove5 = new UserTask('camilux4', false, 4);
+    addAndRemove5.add();
+    addAndRemove.check(2);
+    addAndRemove.check(3);
+    addAndRemove.check(4);
+    addAndRemove.check(5);
+    expect(addAndRemove.btnRemoveChecked()).toBe(4);
+  });
+  test('add elmnts check and remove checkeds elmnts', () => {
+    const addAndRemove1 = new UserTask('vitron1', false, 5);
+    addAndRemove1.add();
+    const addAndRemove2 = new UserTask('vitron2', false, 6);
+    addAndRemove2.add();
+    const addAndRemove3 = new UserTask('vitron3', false, 2);
+    addAndRemove3.add();
+    const addAndRemove4 = new UserTask('vitron4', false, 3);
+    addAndRemove4.add();
+    addAndRemove.check(5);
+    addAndRemove.check(7);
+    addAndRemove.check(1);
+    addAndRemove.check(8);
+    addAndRemove.check(4);
+    addAndRemove.check(9);
+    addAndRemove.check(11);
+    addAndRemove.check(10);
+    expect(addAndRemove.btnRemoveChecked()).toBe(4);
+  });
+  test('remove all checkeds elmnts', () => {
+    addAndRemove.check(0);
+    addAndRemove.check(1);
+    addAndRemove.check(2);
+    expect(addAndRemove.btnRemoveChecked()).toBe(1);
+  });
+});
+
+describe('Test update storage', () => {
+  test('update on description', () => {
+    expect(addAndRemove.updateStore('camilux in the mood', 0)).toBe('camilux in the mood');
+  });
+  test('update on description', () => {
+    expect(addAndRemove.updateStore('camilux in the mood', 3)).toBe(-1);
   });
 });
