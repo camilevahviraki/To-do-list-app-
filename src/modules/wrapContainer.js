d387f74941e5a915c68ed3c3fa638c6260b219b0
camilevahviraki <camilevahviraki@gmail.com>
2023-03-03 15:29:16 +0200

    Update wrapcontainer.js

--- src/modules/wrapContainer.js
+++ src/modules/wrapContainer.js
@@ -1,12 +1,19 @@
 import imgRecycle from '../images/recycle.png';
+import addMore from '../images/close-icon.png';
 
 const WrapContainer = () => {
   document.body.innerHTML = `<div class="container">
    <h3>Today's to do
    <img src=${imgRecycle} id="imgRecycle" alt=""/></h3>
-   <input type="text" placeholder="add your list..." id="inputD" draggable="true"/>
+   <div class="input-add-new-wrapp">
+    <label for='inputD'>
+      <img src=${addMore} alt="" class="icon-add-new"/> 
+    </label>
+    <input type="text" placeholder="Create a new task..." id="inputD" name="inputD"/>
+   </div>
    <ul id="list"></ul>
    <button class="button_clear" id="btnRemove">Clear all selected</button>
+   <p id="error_message"></p>
 </div>`;
 };
 
