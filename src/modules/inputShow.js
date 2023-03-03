249c8342048a6fa121cae9d0c1facbb2ee364734
camilevahviraki <camilevahviraki@gmail.com>
2023-03-03 15:28:52 +0200

    Update inputShow.js

--- src/modules/inputShow.js
+++ src/modules/inputShow.js
@@ -1,19 +1,36 @@
-const showInput = () => {
-  const allList = document.querySelectorAll('.listTasks');
-  allList.forEach((a, i) => {
-    a.addEventListener('mouseover', () => {
-      document.getElementById(i).style.display = 'flex';
-      document.getElementById(`ptask${i}`).style.display = 'none';
-      document.getElementById(`imtrash${i}`).style.display = 'flex';
-      document.getElementById(`imdots${i}`).style.display = 'none';
-    });
-    a.addEventListener('mouseout', () => {
-      document.getElementById(i).style.display = 'none';
-      document.getElementById(`ptask${i}`).style.display = 'flex';
-      document.getElementById(`imtrash${i}`).style.display = 'none';
-      document.getElementById(`imdots${i}`).style.display = 'flex';
-    });
-  });
-};
+class ShowAndHide {
+  constructor() {
+    this.lists = document.querySelectorAll('.pTask');
+    this.imageDots = document.querySelectorAll('.imgTrash');
+  }
 
-export default showInput;
+  static Input = () => {
+    this.lists = document.querySelectorAll('.pTask');
+    this.lists.forEach((p, i) => {
+      p.addEventListener('mouseover', () => {
+        document.getElementById(i).style.display = 'flex';
+        document.getElementById(`ptask${i}`).style.display = 'none';
+      });
+      document.getElementById(i).addEventListener('mouseout', () => {
+        document.getElementById(i).style.display = 'none';
+        document.getElementById(`ptask${i}`).style.display = 'flex';
+      });
+    });
+  }
+
+  static Trash = () => {
+    this.imageDots = document.querySelectorAll('.imgTrash');
+    this.imageDots.forEach((button, i) => {
+      button.addEventListener('mouseover', () => {
+        document.getElementById(`imtrash${i}`).style.display = 'flex';
+        document.getElementById(`imdots${i}`).style.display = 'none';
+      });
+      document.getElementById(`imtrash${i}`).addEventListener('mouseout', () => {
+        document.getElementById(`imtrash${i}`).style.display = 'none';
+        document.getElementById(`imdots${i}`).style.display = 'flex';
+      });
+    });
+  }
+}
+
+export default ShowAndHide;
